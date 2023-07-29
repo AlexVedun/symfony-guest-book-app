6591dc1ad7b87c7ed839512e1b210234dcca151c
Aleksey Yefremenko <alexeyefr@gmail.com>
2023-07-29 16:15:38 +0300

    Add: display the list of moderated messages

--- src/Controller/GuestBookController.php
+++ src/Controller/GuestBookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class GuestBookController extends AbstractController
 {
     #[Route('/', name: 'guest_book_index')]
-    public function index(): Response
+    public function index(WishRepository $wishRepository): Response
     {
+        $wishes = $wishRepository->findBy(['isModerated' => true], ['createdAt' => 'DESC']);
+
         return $this->render('guest_book/index.html.twig', [
-            'controller_name' => 'GuestBookController',
+            'wishes' => $wishes,
         ]);
     }
 }
